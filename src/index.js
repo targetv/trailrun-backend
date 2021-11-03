81353f8effe2ab36c9526727a6e835da7357272c
Sean Davison <seandavison1998@hotmail.com>
2021-11-03 14:27:28 +0000

    Readded orgin

--- src/index.js
+++ src/index.js
@@ -17,8 +17,7 @@ const app = express();
 /* SETUP MIDDLEWARE */
 
 app.disable("x-powered-by");
-// app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
-app.use(cors({ credentials: true }));
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
